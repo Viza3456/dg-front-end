fb45eda9694219453ae2efa3fb16a2de94a4fad2
SbnVda3JGU287nQ* <von.v@adisoft.io>
2025-02-25 15:33:28 +0700

    test

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: '/dg-front-end', // Replace with your GitHub repository name
-  assetPrefix: '/dg-front-end/', // Ensure assets are loaded with the correct prefix
 };
 
 export default nextConfig;
