110f98fd367790065638a6238a813d2fdc50d6b0
SbnVda3JGU287nQ* <von.v@adisoft.io>
2025-02-28 14:52:31 +0700

    update ui

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
+const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/dg-front-end/' : '',
+  basePath: isProd ? '/dg-front-end' : '',
+  output: 'export'
 };
 
 export default nextConfig;
